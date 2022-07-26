927d8eb011bb203f25d4d980a5452ec78af0fea3
Elizaveta Smirnova <matronka@mail.ru>
2022-07-26 13:55:58 +0300

    Add List Description

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export { default as Progress } from './components/Progress/index.js';
 
 export { default as List } from './components/List/index.js';
 export { default as ListOrdered } from './components/ListOrdered/index.js';
+export { default as ListDescription } from './components/ListDescription/index.js';
 
 export { default as Switch } from './components/Switch/index.js';
 
